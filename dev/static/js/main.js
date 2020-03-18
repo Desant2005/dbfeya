e42e2fb91517060aac60e14fb6347fe71f574a31
Alex <arhipmen@gmail.com>
2020-03-18 13:31:04 +0400

    верстка хедера, футера, меню

---
+++ dev/static/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+	svg4everybody({});
+});
+
+// Полифилы
+
+// forEach IE 11
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+	console.info('polyfill for IE11');
+	NodeList.prototype.forEach = function(callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
+// closest IE 11
+(function() {
+	if (!Element.prototype.closest) {
+		Element.prototype.closest = function(css) {
+			var node = this;
+			while (node) {
+				if (node.matches(css)) return node;
+				else node = node.parentElement;
+			}
+			return null;
+		};
+	}
+})();
+
+// matches IE 11
+(function() {
+	if (!Element.prototype.matches) {
+		Element.prototype.matches =
+			Element.prototype.matchesSelector ||
+			Element.prototype.webkitMatchesSelector ||
+			Element.prototype.mozMatchesSelector ||
+			Element.prototype.msMatchesSelector;
+	}
+})();
+
+//Array.form IE 11
+if (!Array.from) {
+	Array.from = function(object) {
+		'use strict';
+		return [].slice.call(object);
+	};
+}
+
+$(document).ready(function() {
+	//animation nav item
+	$('.nav__link').hover(
+		function() {
+			$(this).addClass('active');
+		},
+		function() {
+			$(this).removeClass('active');
+		}
+	);
+	//animation menu left
+
+	$('.item__link').click(function() {
+		$(this)
+			.next('.sub-nav')
+			.slideToggle(500);
+	});
+});
